aca6535267e38498471e1ab25d1942b5e2348528
Harald Steinlechner <haraldsteinlechner@gmail.com>
2014-11-06 13:33:07 +0100

    input validation

--- flit.fs
+++ flit.fs
@@ -4,34 +4,42 @@
 #endif
 
 open FSharp.CLI
+open System.IO
+open System.Collections.Generic
 
 type LiterateCLI = {
     
     [<NoSwitch>]
-    targets : list<string>
+    source : string
 
-    [<Description("output file (either .fs | .tex) ")>]
+    [<Description("output files (either .fs | .tex) ")>]
     [<Switches("o", "output")>]
-    output : string
+    output : list<string>
 }
 
 let usage, parser = usageAndParser<LiterateCLI>
 
+let extension (file : string) = Path.GetExtension file
+let endsWith (suffix : string) (s : string) = s.EndsWith suffix
 
+let toFile (fileName : string) (content : string) = File.WriteAllText (fileName,content)
 
-let extractCode (settings : LiterateCLI) = 
-    failwith ""
-let extractTex (settings : LiterateCLI) = 
-    failwith ""
+let convertTo (sourceFile : string) (targetFile  : string) =
+    "" 
+
 
 [<EntryPoint>]
 let main args = 
     let pUsage () = printfn "usage %s" usage; 2
     match parser args with
         | Some settings -> 
-            if settings.output.EndsWith ".fs"
-            then extractCode settings
-            elif settings.output.EndsWith ".tex"
-            then extractTex settings
-            else pUsage () 
+            let validExtension s = extension s = "fs" || extension s = "tex" 
+            if settings.output |> Seq.exists (not << validExtension) 
+            then
+                printfn "unsupported file ending."
+                pUsage()
+            else
+                for o in settings.output do
+                    convertTo settings.source o |> toFile o
+                0
         | None -> pUsage () 
